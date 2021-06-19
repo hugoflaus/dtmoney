3c069f428d3e80b406937cecdb5b1954bddf03c3
Hugo Reis <hugo.reis@localiza.com>
2021-06-19 18:17:02 -0300

    Create buttons income and outcome at modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -31,6 +33,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           placeholder="Valor" />
 
         <input placeholder="Categoria" />
+
+        <TransactionTypeContainer>
+          <button
+            type="button">
+              <img src={incomeImg} alt="Entrada"/>
+              <span>Entrada</span>
+          </button>
+          <button
+            type="button">
+              <img src={outcomeImg} alt="Saida"/>
+              <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
 
         <button type="submit">
           Cadastrar
